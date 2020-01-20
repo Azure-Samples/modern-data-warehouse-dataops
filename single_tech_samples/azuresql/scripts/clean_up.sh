3fdb903b8630cb1ae21b8627ced8483e900965c5
devlace <lace.lofranco@microsoft.com>
2020-01-20 16:24:54 +1000

    feat: add azuresql samples - cleanup script (#35)

--- single_tech_samples/azuresql/scripts/clean_up.sh
+++ single_tech_samples/azuresql/scripts/clean_up.sh
@@ -5,3 +5,6 @@ az pipelines list -o tsv | grep "azuresql-" | awk '{print $4}' | xargs -I % az p
 
 # Delete service connections
 az devops service-endpoint list -o tsv | grep "mdw-dataops" | awk '{print $3}' | xargs -I % az devops service-endpoint delete --id % --yes
+
+# Delete service principal
+az ad sp list --query "[?contains(appDisplayName,'sp_dataops_')].appId" -o tsv --show-mine | xargs -I % az ad sp delete --id %
