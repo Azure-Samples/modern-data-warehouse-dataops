5ae095a376627532d5bf38c3cf966a6f578c135c
devlace <lace.lofranco@microsoft.com>
2020-02-27 22:32:35 +1000

    AzureSQL: Shorten deploy id and names (#52)
    * Update deploy id and names
    
    * Update azure-pipelines-03-simple-multi-stage.yml for Azure Pipelines
    
    * update to short prefix
    
    * chore: remove debugging flag

--- single_tech_samples/azuresql/scripts/clean_up.sh
+++ single_tech_samples/azuresql/scripts/clean_up.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
-echo "Delete pipelines with 'mdw-dataops-' in name..."
-az pipelines list -o tsv | grep "mdw-dataops-" | awk '{print $4}' | xargs -I % az pipelines delete --id % --yes
+echo "Delete pipelines with 'mdwdo-azsql' in name..."
+az pipelines list -o tsv | grep "mdwdo-azsql" | awk '{print $4}' | xargs -I % az pipelines delete --id % --yes
 
-echo "Delete service connections..."
-az devops service-endpoint list -o tsv | grep "mdw-dataops" | awk '{print $3}' | xargs -I % az devops service-endpoint delete --id % --yes
+echo "Delete service connections with 'mdwdo-azsql' in name..."
+az devops service-endpoint list -o tsv | grep "mdwdo-azsql" | awk '{print $3}' | xargs -I % az devops service-endpoint delete --id % --yes
 
-echo "Delete service principal..."
-az ad sp list --query "[?contains(appDisplayName,'mdw-dataops')].appId" -o tsv --show-mine | xargs -I % az ad sp delete --id %
+echo "Delete service principal with 'mdwdo-azsql' in name, created by yourself..."
+az ad sp list --query "[?contains(appDisplayName,'mdwdo-azsql')].appId" -o tsv --show-mine | xargs -I % az ad sp delete --id %
+
+echo "Delete resource group with 'mdw-dataops-azuresq' in name..."
+az group list --query "[?contains(name,'mdwdo-azsql')].name" -o tsv | xargs -I % az group delete --name % -y --no-wait
