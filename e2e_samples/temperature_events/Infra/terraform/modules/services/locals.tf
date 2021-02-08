e93809cacdc0aa90a60f1644b3709ad3582a370b
Akira Kakkar <akira.kakkar@microsoft.com>
2021-02-08 22:53:51 +0900

    Add subscription id in azure function config

--- e2e_samples/temperature_events/Infra/terraform/modules/services/locals.tf
+++ e2e_samples/temperature_events/Infra/terraform/modules/services/locals.tf
@@ -3,5 +3,6 @@ locals {
   connection_string   = tomap({ for item in var.event_hub_names : "${item}EvhConnection" => "@Microsoft.KeyVault(VaultName=kv-${local.resource_name};SecretName=${item}-conn;SecretVersion=)" })
   eventhub_names      = tomap({ for i, name in module.eventhubs : "${var.event_hub_names[i]}Evh" => name.eventhub_name })
   instrumentation_key = tomap({ APPINSIGHTS_INSTRUMENTATIONKEY = "@Microsoft.KeyVault(VaultName=kv-${local.resource_name};SecretName=${var.app_insights_name};SecretVersion=)" })
-  appsettings         = merge(local.connection_string, local.eventhub_names, local.instrumentation_key)
+  subscription_id     = tomap({ subscription_id = data.azurerm_client_config.current.subscription_id })
+  appsettings         = merge(local.connection_string, local.eventhub_names, local.instrumentation_key, local.subscription_id)
 }
