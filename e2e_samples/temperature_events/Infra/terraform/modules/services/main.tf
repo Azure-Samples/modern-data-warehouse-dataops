ad77a2a1cb8f3c6c3dc7d83d406c1ec7435867a2
Akira Kakkar <akira.kakkar@microsoft.com>
2021-02-10 16:52:43 +0900

    fixed syntax error for the name of keyvault

--- e2e_samples/temperature_events/Infra/terraform/modules/services/main.tf
+++ e2e_samples/temperature_events/Infra/terraform/modules/services/main.tf
@@ -101,8 +101,8 @@ resource "azurerm_key_vault_secret" "kv_appinsights_conn_string" {
   depends_on   = [azurerm_key_vault_access_policy.keyvault_policy, azurerm_application_insights.app_insights, azurerm_key_vault_access_policy.azurerm_client_keyvault_policy]
 }
 
-resource "azurerm_key_vault_secret" "kv_subsciption_id" {
-  name         = "subscription_id"
+resource "azurerm_key_vault_secret" "kv_subscription_id" {
+  name         = "subscription-id"
   value        = data.azurerm_client_config.current.subscription_id
   key_vault_id = azurerm_key_vault.kv.id
 }
