715132d9615975e95c0cb732fbc7da23a02e2b98
Jacob Zhou <jazhou@microsoft.com>
2021-03-11 03:51:41 +0000

    Adding validation and create

--- single_tech_samples/databricks/Cluster_Deployment/deploy.sh
+++ single_tech_samples/databricks/Cluster_Deployment/deploy.sh
@@ -1,9 +1,22 @@
 #!/usr/bin/env bash
 
+random_str() {
+    local length=$1
+    cat /dev/urandom | tr -dc 'a-z' | fold -w "$length" | head -n 1
+    return 0
+}
+
+DEPLOYMENT_PREFIX=${DEPLOYMENT_PREFIX:-}
 AZURE_SUBSCRIPTION_ID=${AZURE_SUBSCRIPTION_ID:-}
 AZURE_RESOURCE_GROUP_NAME=${AZURE_RESOURCE_GROUP_NAME:-}
 AZURE_RESOURCE_GROUP_LOCATION=${AZURE_RESOURCE_GROUP_LOCATION:-}
 
+if [[ -z "$DEPLOYMENT_PREFIX" ]]
+then
+    echo "No [DEPLOYMENT_PREFIX] specified, generating one."
+    DEPLOYMENT_PREFIX=$(random_str 3)
+    echo "[DEPLOYMENT_PREFIX] is set to \"$DEPLOYMENT_PREFIX\"."
+fi
 if [[ -z "$AZURE_SUBSCRIPTION_ID" ]]
 then
     echo "No Azure subscription id [AZURE_SUBSCRIPTION_ID] specified."
@@ -47,7 +60,90 @@ fi
 
 # Validate the ARM templates (Jacob)
 
+tagValues="{}"
+
+disablePublicIp=false
+adbWorkspaceLocation="$AZURE_RESOURCE_GROUP_LOCATION"
+adbWorkspaceName="${DEPLOYMENT_PREFIX}adb01"
+adbWorkspaceSkuTier="standard"
+az deployment group validate \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./databricks/workspace.template.json \
+    --parameters \
+        disablePublicIp="$disablePublicIp" \
+        adbWorkspaceLocation="$adbWorkspaceLocation" \
+        adbWorkspaceName="$adbWorkspaceName" \
+        adbWorkspaceSkuTier="$adbWorkspaceSkuTier" \
+        tagValues="$tagValues"
+
+keyVaultName="${DEPLOYMENT_PREFIX}akv01"
+keyVaultLocation="$AZURE_RESOURCE_GROUP_LOCATION"
+enabledForDeployment="false"
+enabledForTemplateDeployment="false"
+tenantId="$(az account show --query "tenantId" --output tsv)"
+objectId="$(az ad signed-in-user show --query "objectId" --output tsv)"
+keyVaultSkuTier="Standard"
+az deployment group validate \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./keyvault/keyvault.template.json \
+    --parameters \
+        keyVaultName="$keyVaultName" \
+        keyVaultLocation="$keyVaultLocation" \
+        enabledForDeployment="$enabledForDeployment" \
+        enabledForTemplateDeployment="$enabledForTemplateDeployment" \
+        tenantId="$tenantId" \
+        objectId="$objectId" \
+        keyVaultSkuTier="$keyVaultSkuTier" \
+        tagValues="$tagValues"
+
+storageAccountName="${DEPLOYMENT_PREFIX}asa01"
+storageAccountSku="Standard_LRS"
+storageAccountSkuTier="Standard"
+storageAccountLocation="$AZURE_RESOURCE_GROUP_LOCATION"
+encryptionEnabled="true"
+az deployment group validate \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./storageaccount/storageaccount.template.json \
+    --parameters \
+        storageAccountName="$storageAccountName" \
+        storageAccountSku="$storageAccountSku" \
+        storageAccountSkuTier="$storageAccountSkuTier" \
+        storageAccountLocation="$storageAccountLocation" \
+        encryptionEnabled="$encryptionEnabled"
+
 # Deploy ARM templates (Jacob)
+az deployment group create \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./databricks/workspace.template.json \
+    --parameters \
+        disablePublicIp="$disablePublicIp" \
+        adbWorkspaceLocation="$adbWorkspaceLocation" \
+        adbWorkspaceName="$adbWorkspaceName" \
+        adbWorkspaceSkuTier="$adbWorkspaceSkuTier" \
+        tagValues="$tagValues"
+
+az deployment group create \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./keyvault/keyvault.template.json \
+    --parameters \
+        keyVaultName="$keyVaultName" \
+        keyVaultLocation="$keyVaultLocation" \
+        enabledForDeployment="$enabledForDeployment" \
+        enabledForTemplateDeployment="$enabledForTemplateDeployment" \
+        tenantId="$tenantId" \
+        objectId="$objectId" \
+        keyVaultSkuTier="$keyVaultSkuTier" \
+        tagValues="$tagValues"
+
+az deployment group create \
+    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+    --template-file ./storageaccount/storageaccount.template.json \
+    --parameters \
+        storageAccountName="$storageAccountName" \
+        storageAccountSku="$storageAccountSku" \
+        storageAccountSkuTier="$storageAccountSkuTier" \
+        storageAccountLocation="$storageAccountLocation" \
+        encryptionEnabled="$encryptionEnabled"
 
 # Configure Key Vault access policy (Juan)
 
