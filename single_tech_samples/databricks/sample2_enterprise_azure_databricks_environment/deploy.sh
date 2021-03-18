13606122676c00abb421c7d4a72578d24d552bc1
Juan Burckhardt <jsburckhardt@gmail.com>
2021-03-18 23:53:01 +1100

    feat(privete-link): setup up private link

--- single_tech_samples/databricks/sample2_enterprise_azure_databricks_environment/deploy.sh
+++ single_tech_samples/databricks/sample2_enterprise_azure_databricks_environment/deploy.sh
@@ -31,7 +31,7 @@ hubVnetName="${DEPLOYMENT_PREFIX}hubVnet01"
 vnetLocation="$AZURE_RESOURCE_GROUP_LOCATION"
 
 tagValues="{}"
-disablePublicIp=false
+disablePublicIp=true
 adbWorkspaceLocation="$AZURE_RESOURCE_GROUP_LOCATION"
 adbWorkspaceName="${DEPLOYMENT_PREFIX}adb01"
 adbWorkspaceSkuTier="standard"
@@ -56,6 +56,8 @@ routeTableName="${DEPLOYMENT_PREFIX}FWRT01"
 firewallName="${DEPLOYMENT_PREFIX}HubFW01"
 iPAddressName="${DEPLOYMENT_PREFIX}FWIP01"
 firewalllocation="$AZURE_RESOURCE_GROUP_LOCATION"
+
+scopeName="storage_scope"
 
 # Login to Azure and select the subscription
 if ! AZURE_USERNAME=$(az account show --query user.name); then
@@ -173,6 +175,7 @@ if ! az deployment group validate \
         objectId="$objectId" \
         keyVaultSkuTier="$keyVaultSkuTier" \
         tagValues="$tagValues" \
+        spokeVirtualNetworkId='value' \
     --output none; then
     echo "Validation error for Azure Key Vault, please see the error above."
     exit 1
@@ -189,7 +192,7 @@ if
             keyvaultPrivateLinkResource="somelinkresource" \
             targetSubResource="vault" \
             keyvaultName="$keyVaultName" \
-            vnetName="$vnetName" \
+            vnetName="$spokeVnetName" \
             privateLinkSubnetId="somelinkresource" \
         --output none
 then
@@ -198,12 +201,6 @@ then
 else
     echo "Azure Key Vault Privatelink parameters are valid."
 fi
-
-storageAccountName="${DEPLOYMENT_PREFIX}asa01"
-storageAccountSku="Standard_LRS"
-storageAccountSkuTier="Standard"
-storageAccountLocation="$AZURE_RESOURCE_GROUP_LOCATION"
-encryptionEnabled="true"
 
 echo "Validating parameters for Azure Storage Account..."
 if ! az deployment group validate \
@@ -231,7 +228,7 @@ if
             storageAccountPrivateLinkResource="vault" \
             storageAccountName="$storageAccountName" \
             targetSubResource="dfs" \
-            vnetName="$vnetName" \
+            vnetName="$spokeVnetName" \
             privateLinkSubnetId="somelinkresource" \
         --output none
 then
@@ -243,7 +240,7 @@ fi
 
 echo "******Starting deployments... ********"
 echo "Deploying Network Security Group..."
-nsg_arm_output=$(az deployment group create \
+nsgArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./securitygroup/securitygroup.template.json \
     --parameters \
@@ -251,13 +248,13 @@ nsg_arm_output=$(az deployment group create \
         securityGroupLocation="$securityGroupLocation" \
     --output json)
 
-if [[ -z $nsg_arm_output ]]; then
+if [[ -z $nsgArmOutput ]]; then
     echo >&2 "Network Security Group ARM deployment failed."
     exit 1
 fi
 
 echo "Deploying Route Table..."
-art_arm_output=$(az deployment group create \
+artArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./routetable/routetable.template.json \
     --parameters \
@@ -265,13 +262,13 @@ art_arm_output=$(az deployment group create \
         reouteTablelocation="$reouteTablelocation" \
     --output json)
 
-if [[ -z $art_arm_output ]]; then
+if [[ -z $artArmOutput ]]; then
     echo >&2 "Route Table ARM deployment failed."
     exit 1
 fi
 
 echo "Deploying Virtual Network..."
-vnet_arm_output=$(az deployment group create \
+vnetArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./vnet/vnet.template.json \
     --parameters \
@@ -282,13 +279,13 @@ vnet_arm_output=$(az deployment group create \
         routeTableName="$routeTableName" \
     --output json)
 
-if [[ -z $vnet_arm_output ]]; then
+if [[ -z $vnetArmOutput ]]; then
     echo >&2 "Virtual Network ARM deployment failed."
     exit 1
 fi
 
 echo "Deploying Firewall..."
-afw_arm_output=$(az deployment group create \
+afwArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./firewall/firewall.template.json \
     --parameters \
@@ -298,13 +295,13 @@ afw_arm_output=$(az deployment group create \
         vnetName="$hubVnetName" \
     --output json)
 
-if [[ -z $afw_arm_output ]]; then
+if [[ -z $afwArmOutput ]]; then
     echo >&2 "Firewall ARM deployment failed."
     exit 1
 fi
 
 echo "Deploying Azure Databricks Workspace..."
-adbws_arm_output=$(az deployment group create \
+adbwsArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./databricks/workspace.template.json \
     --parameters \
@@ -317,13 +314,64 @@ adbws_arm_output=$(az deployment group create \
         adbWorkspaceSkuTier=premium \
     --output json)
 
-if [[ -z $adbws_arm_output ]]; then
+if [[ -z $adbwsArmOutput ]]; then
     echo >&2 "Databricks ARM deployment failed."
     exit 1
 fi
 
+echo "Deploying Azure Storage Account..."
+spokeVirtualNetworkId="$(echo "$vnetArmOutput" | jq -r '.properties.outputs.spoke_vnet_id.value')"
+networkAclsVirtualNetworkRules="[
+    {
+        \"id\": \"$spokeVirtualNetworkId/subnets/private-subnet\"
+    },
+    {
+        \"id\": \"$spokeVirtualNetworkId/subnets/privatelink-subnet\"        
+    },
+    {
+        \"id\": \"$spokeVirtualNetworkId/subnets/public-subnet\"        
+    }
+]"
+asaArmOutput=$(
+        az deployment group create \
+        --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+        --template-file ./storageaccount/storageaccount.template.json \
+        --parameters \
+            storageAccountName="$storageAccountName" \
+            storageAccountSku="$storageAccountSku" \
+            storageAccountSkuTier="$storageAccountSkuTier" \
+            storageAccountLocation="$storageAccountLocation" \
+            encryptionEnabled="$encryptionEnabled" \
+            networkAclsVirtualNetworkRules="$networkAclsVirtualNetworkRules" \
+        --output json
+)
+
+if [[ -z $asaArmOutput ]]; then
+    echo >&2 "Storage Account ARM deployment failed."
+    exit 1
+fi
+
+echo "Deploying Storage Account Private Link..."
+if
+    ! az deployment group create \
+        --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
+        --template-file ./storageaccount/privateendpoint.template.json \
+        --parameters \
+            storageAccountPrivateLinkResource="$(echo "$asaArmOutput" | jq -r '.properties.outputs.storageaccount_id.value')" \
+            storageAccountName="$storageAccountName" \
+            targetSubResource="dfs" \
+            vnetName="$spokeVnetName" \
+            privateLinkSubnetId="$(echo "$vnetArmOutput" | jq -r '.properties.outputs.privatelinksubnet_id.value')" \
+        --output none
+then
+    echo "Deployment of Azure Storage Account failed, please see the error above."
+    exit 1
+else
+    echo "Deployment of Azure Storage Account succeeded."
+fi
+
 echo "Deploying Azure Key Vault..."
-akv_arm_output=$(az deployment group create \
+akvArmOutput=$(az deployment group create \
     --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
     --template-file ./keyvault/keyvault.template.json \
     --parameters \
@@ -335,11 +383,28 @@ akv_arm_output=$(az deployment group create \
         objectId="$objectId" \
         keyVaultSkuTier="$keyVaultSkuTier" \
         tagValues="$tagValues" \
+        spokeVirtualNetworkId="$(echo "$vnetArmOutput" | jq -r '.properties.outputs.spoke_vnet_id.value')" \
     --output json)
-if [[ -z $akv_arm_output ]]; then
+if [[ -z $akvArmOutput ]]; then
     echo >&2 "Key Vault ARM deployment failed."
     exit 1
 fi
+
+# Store Storage Account keys in Key Vault
+echo "Adding local IP into ACL while storing SA secrets (before private link config) ...."
+az keyvault network-rule add --resource-group "$AZURE_RESOURCE_GROUP_NAME" --name "$keyVaultName" --ip-address "$(curl ifconfig.me)" --output none
+echo "Retrieving keys from storage account"
+storageKeys=$(az storage account keys list --resource-group "$AZURE_RESOURCE_GROUP_NAME" --account-name "$storageAccountName")
+storageAccountKey1=$(echo "$storageKeys" | jq -r '.[0].value')
+storageAccountKey2=$(echo "$storageKeys" | jq -r '.[1].value')
+
+echo "Storing keys in key vault"
+az keyvault secret set -n "StorageAccountKey1" --vault-name "$keyVaultName" --value "$storageAccountKey1" --output none
+az keyvault secret set -n "StorageAccountKey2" --vault-name "$keyVaultName" --value "$storageAccountKey2" --output none
+echo "Successfully stored secrets StorageAccountKey1 and StorageAccountKey2"
+echo "Removing local IP into ACL while storing secrets ...."
+az keyvault network-rule remove --resource-group "$AZURE_RESOURCE_GROUP_NAME" --name "$keyVaultName" --ip-address "$(curl ifconfig.me)" --output none
+echo "Removed"
 
 echo "Deploying Keyvault Private Link..."
 if
@@ -347,11 +412,11 @@ if
         --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
         --template-file ./keyvault/privateendpoint.template.json \
         --parameters \
-            keyvaultPrivateLinkResource="$(echo "$akv_arm_output" | jq -r '.properties.outputs.keyvault_id.value')" \
+            keyvaultPrivateLinkResource="$(echo "$akvArmOutput" | jq -r '.properties.outputs.keyvault_id.value')" \
             targetSubResource="vault" \
             keyvaultName="$keyVaultName" \
-            vnetName="$vnetName" \
-            privateLinkSubnetId="$(echo "$vnet_arm_output" | jq -r '.properties.outputs.privatelinksubnet_id.value')" \
+            vnetName="$spokeVnetName" \
+            privateLinkSubnetId="$(echo "$vnetArmOutput" | jq -r '.properties.outputs.privatelinksubnet_id.value')" \
         --output none
 then
     echo "Deployment of Keyvault Private Link failed, please see the error above."
@@ -360,107 +425,35 @@ else
     echo "Deployment of Keyvault Private Link succeeded."
 fi
 
-echo "Deploying Azure Storage Account..."
-asa_arm_output=$(az deployment group create \
-    --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
-    --template-file ./storageaccount/storageaccount.template.json \
-    --parameters \
-        storageAccountName="$storageAccountName" \
-        storageAccountSku="$storageAccountSku" \
-        storageAccountSkuTier="$storageAccountSkuTier" \
-        storageAccountLocation="$storageAccountLocation" \
-        encryptionEnabled="$encryptionEnabled" \
-    --output json)
+# Create ADB secret scope backed by Key Vault
+adbGlobalToken=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d --output json | jq -r .accessToken)
+echo "Got adbGlobalToken=\"${adbGlobalToken:0:20}...${adbGlobalToken:(-20)}\""
+azureApiToken=$(az account get-access-token --resource https://management.core.windows.net/ --output json | jq -r .accessToken)
+echo "Got azureApiToken=\"${azureApiToken:0:20}...${azureApiToken:(-20)}\""
 
-if [[ -z $asa_arm_output ]]; then
-    echo >&2 "Storage Account ARM deployment failed."
-    exit 1
-fi
+keyVaultId=$(echo "$akvArmOutput" | jq -r '.properties.outputs.keyvault_id.value')
+keyVaultUri=$(echo "$akvArmOutput" | jq -r '.properties.outputs.keyvault_uri.value')
 
-echo "Deploying Storage Account Private Link..."
-if
-    ! az deployment group create \
-        --resource-group "$AZURE_RESOURCE_GROUP_NAME" \
-        --template-file ./storageaccount/privateendpoint.template.json \
-        --parameters \
-            storageAccountPrivateLinkResource="$(echo "$asa_arm_output" | jq -r '.properties.outputs.storageaccount_id.value')" \
-            storageAccountName="$storageAccountName" \
-            targetSubResource="dfs" \
-            vnetName="$vnetName" \
-            privateLinkSubnetId="$(echo "$vnet_arm_output" | jq -r '.properties.outputs.privatelinksubnet_id.value')" \
-        --output none
-then
-    echo "Deployment of Azure Storage Account failed, please see the error above."
-    exit 1
-else
-    echo "Deployment of Azure Storage Account succeeded."
-fi
+adbId=$(az databricks workspace show --resource-group "$AZURE_RESOURCE_GROUP_NAME" --name "$adbWorkspaceName" --query id --output tsv)
+adbWorkspaceUrl=$(az databricks workspace show --resource-group "$AZURE_RESOURCE_GROUP_NAME" --name "$adbWorkspaceName" --query workspaceUrl --output tsv)
 
-# ###########################
-# # RETRIEVE DATABRICKS INFORMATION AND GENERATE TOKEN
-echo "Retrieving Databricks information from the deployment."
-databricks_workspace_id=$(echo "$adbws_arm_output" | jq -r '.properties.outputs.databricks_workspace_id.value')
-DATABRICKS_HOST=https://$(echo "$adbws_arm_output" | jq -r '.properties.outputs.databricks_workspace.value.workspaceUrl')
-export DATABRICKS_HOST
+authHeader="Authorization: Bearer $adbGlobalToken"
+adbSPMgmtToken="X-Databricks-Azure-SP-Management-Token:$azureApiToken"
+adbResourceId="X-Databricks-Azure-Workspace-Resource-Id:$adbId"
 
-# Retrieve databricks PAT token
-echo "Generating a Databricks PAT token."
-databricks_global_token=$(az account get-access-token --resource 2ff814a6-3304-4ab8-85cb-cd0e6f879c1d --output json | jq -r .accessToken) # Databricks app global id
-azure_api_token=$(az account get-access-token --resource https://management.core.windows.net/ --output json | jq -r .accessToken)
-api_response=$(curl -sf "$DATABRICKS_HOST"/api/2.0/token/create \
-    -H "Authorization: Bearer $databricks_global_token" \
-    -H "X-Databricks-Azure-SP-Management-Token:$azure_api_token" \
-    -H "X-Databricks-Azure-Workspace-Resource-Id:$databricks_workspace_id" \
-    -d '{ "comment": "For deployment" }')
-databricks_token=$(echo "$api_response" | jq -r '.token_value')
-export DATABRICKS_TOKEN=$databricks_token
+createSecretScopePayload="{
+    \"scope\": \"$scopeName\",
+    \"scope_backend_type\": \"AZURE_KEYVAULT\",
+    \"backend_azure_keyvault\":
+    {
+    \"resource_id\": \"$keyVaultId\",
+    \"dns_name\": \"$keyVaultUri\"
+    },
+    \"initial_manage_principal\": \"users\"
+}"
+echo "$createSecretScopePayload" | curl -sS -X POST -H "$authHeader" -H "$adbSPMgmtToken" -H "$adbResourceId" \
+    --data-binary "@-" "https://${adbWorkspaceUrl}/api/2.0/secrets/scopes/create"
 
-echo "Waiting for Databricks workspace to be ready..."
-# TODO: -> sleep 3m # if the cluster is going to get created after the token is in there ... need to add the sleep 3.
-echo "DATABRICKS token generated"
-
-# Store token in Key Vault (Juan)
-echo "Storing DATABRICKS token secret in Key Vault."
-az keyvault secret set -n "DatabricksDeploymentToken" --vault-name "$keyVaultName" --value "$DATABRICKS_TOKEN" --output none
-echo "Successfully stored secret DatabricksDeploymentToken"
-
-# Store Storage Account keys in Key Vault (Juan)
-echo "Retrieving keys from storage account"
-storage_keys=$(az storage account keys list --resource-group "$AZURE_RESOURCE_GROUP_NAME" --account-name "$storageAccountName")
-storage_account_key1=$(echo "$storage_keys" | jq -r '.[0].value')
-storage_account_key2=$(echo "$storage_keys" | jq -r '.[1].value')
-
-echo "Storing keys in key vault"
-az keyvault secret set -n "StorageAccountKey1" --vault-name "$keyVaultName" --value "$storage_account_key1" --output none
-az keyvault secret set -n "StorageAccountKey2" --vault-name "$keyVaultName" --value "$storage_account_key2" --output none
-echo "Successfully stored secrets StorageAccountKey1 and StorageAccountKey2"
-
-##### Create ADB secret scope
-# TODO: validate scope create with kv as backend issue https://github.com/DataThirstLtd/azure.databricks.cicd.tools/issues/43
-# key_vault_id=$(echo $akv_arm_output | jq -r '.properties.outputs.keyvault_id.value')
-# key_vault_uri=$(echo $akv_arm_output | jq -r '.properties.outputs.keyvault_uri.value')
-# databricks secrets create-scope \
-#     --scope deploymentsecretscope \
-#     --scope-backend-type AZURE_KEYVAULT \
-#     --initial-manage-principal users \
-#     --resource-id $key_vault_id \
-#     --dns-name $key_vault_uri
-# TODO: use a sp to connect to SA instead of keys
-# for now we will be creating the secret scope using the ADP token
-scope_name="storage_scope"
-if ! (databricks secrets list-scopes |  grep -q "$scope_name"); then
-    echo "Creating secrets scope: $scope_name"
-    # including --initial-manage-principal users as adb ws sku == Standard
-    databricks secrets create-scope --scope "$scope_name" --initial-manage-principal "users"
-fi
-
-# Create secrets
-echo "Creating secrets within scope $scope_name..."
-
-databricks secrets write --scope "$scope_name" --key "storage_account_key1" --string-value  "$storage_account_key1"
-databricks secrets write --scope "$scope_name" --key "storage_account_key2" --string-value  "$storage_account_key2"
-
-###############################
 # Summary or resources created:
 printf "\n\n\nRESOURCE GROUP: \t\t %s\n" "$AZURE_RESOURCE_GROUP_NAME"
 printf "STORAGE ACCOUNT: \t\t %s\n" "$storageAccountName"
