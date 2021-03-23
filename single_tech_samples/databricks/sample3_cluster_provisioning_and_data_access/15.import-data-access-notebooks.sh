daaf7693d67ae0ad94e9a3e99249586f9442923c
Jacob Zhou <jazhou@microsoft.com>
2021-03-23 05:38:50 +0000

    Adding import statements

--- single_tech_samples/databricks/sample3_cluster_provisioning_and_data_access/15.import-data-access-notebooks.sh
+++ single_tech_samples/databricks/sample3_cluster_provisioning_and_data_access/15.import-data-access-notebooks.sh
@@ -2,5 +2,27 @@
 
 set -e
 
-databricks workspace import -l PYTHON -f JUPYTER -o ./access-data-via-mount.ipy /access-data-via-mount
-databricks workspace import -l PYTHON -f JUPYTER -o ./access-data-ad-passthrough.ipy /access-data-ad-passthrough
+DEPLOYMENT_PREFIX=${DEPLOYMENT_PREFIX:-}
+
+if [[ -z "$DEPLOYMENT_PREFIX" ]]; then
+    echo "No deployment prefix [DEPLOYMENT_PREFIX] specified."
+    exit 1
+fi
+
+# Import the notebooks
+
+mountTmpFile="./access-data-via-mount.tmp.ipy"
+passthroughTmpFile="./access-data-ad-passthrough.tmp.ipy"
+
+sed -e "s/\${DEPLOYMENT_PREFIX}/$DEPLOYMENT_PREFIX/g" < ./access-data-via-mount.ipy > "$mountTmpFile"
+sed -e "s/\${DEPLOYMENT_PREFIX}/$DEPLOYMENT_PREFIX/g" < ./access-data-ad-passthrough.ipy > "$passthroughTmpFile"
+
+databricks workspace import -l PYTHON -f SOURCE -o "$mountTmpFile" /access-data-via-mount
+databricks workspace import -l PYTHON -f SOURCE -o "$passthroughTmpFile" /access-data-ad-passthrough
+
+cat <<EOF
+Notebook "access-data-via-mount" and "access-data-ad-passthrough" have been uploaded to the workspace.
+Please go to the Databricks workspace and run the notebooks using the corresponding cluster.
+- via mount => use default cluster
+- via ad passthrough => use high concurrency cluster
+EOF
