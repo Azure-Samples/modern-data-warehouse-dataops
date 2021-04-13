455afc6f3e961d7611eb16ec9b7e01b5c4542f66
Akira Kakkar <akira.kakkar@microsoft.com>
2021-04-13 16:09:20 +0000

    Fixed unittest

--- e2e_samples/dataset_versioning/insert_sql/tests/test_main.py
+++ e2e_samples/dataset_versioning/insert_sql/tests/test_main.py
@@ -17,11 +17,11 @@ def test_missing_value(mocker):
 @pytest.mark.parametrize(
     "test_input, expected",
     [(["-c", "-k", "keyvault"], [True, False, False]),
-     (["-k", "keyvault", "-v", 0, "-p", "./test_sample.csv"], [False, False, True])])
+     (["-k", "keyvault", "-v", 0, "-p", "./test_sample.csv"], [False, True, True])])
 def test_different_valid_arguments(mocker, test_input, expected):
     sql_mock = mocker.Mock(spec=SqlWrapper)
     keyvault_mock = mocker.Mock(spec=KeyvaultWrapper)
     process_mock = mocker.Mock(spec=Process)
     main(keyvault_mock, sql_mock, process_mock, test_input)
-    actual_result = [sql_mock().clean_up.called, process_mock().called, sql_mock().insert_to_sql.called]
+    actual_result = [sql_mock().clean_up.called, process_mock().filter_with_version.called, sql_mock().insert_to_sql.called]
     assert actual_result == expected
