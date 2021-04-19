df82f29c968bc46024cf1ac3447db97c50b4d0af
Akira Kakkar <akira.kakkar@microsoft.com>
2021-04-19 01:36:44 +0000

    Add pipeline and updated setup.sh

--- e2e_samples/dataset_versioning/infra/setup.sh
+++ e2e_samples/dataset_versioning/infra/setup.sh
@@ -1,96 +1,81 @@
 #!/bin/bash
 
-# Access granted under MIT Open Source License: https://en.wikipedia.org/wiki/MIT_License
-#
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
-# documentation files (the "Software"), to deal in the Software without restriction, including without limitation
-# the rights to use, copy, modify, merge, publish, distribute, sublicense, # and/or sell copies of the Software,
-# and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-#
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions
-# of the Software.
-#
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED
-# TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
-# THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF
-# CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
-# DEALINGS IN THE SOFTWARE.
+while true; do
+    echo "Enter the name for 'resource group', 'storage account', 'keyvault', and 'location', separated by spaces."
+    echo -e "ex:\n    resourcegroup storageaccount kv-terraform-prime eastus \n"
+    echo -e "Then, following resources will be created:"
+    echo "    Resource Group: resourcegroup"
+    echo "    Storage Account: storageaccount"
+    echo "    Keyvault: kv-terraform-prime"
+    echo "    Service Principal: sp-resourcegroup-Prime-tf"
+    echo -e "\n"
+    read -p "Enter value: " RESOURCE_GROUP_NAME TF_STATE_STORAGE_ACCOUNT_NAME KEYVAULT_NAME LOCATION
 
-set -o errexit
-set -o pipefail
-set -o nounset
-# set -o xtrace # For debugging
+    SP_NAME="sp-${RESOURCE_GROUP_NAME}-tf"
+    TF_STATE_CONTAINER_NAME=terraform-state
 
-if [ $# -ne 4 ]; then
-    cat << EOF
-Usage:
+    # Create the resource group
+    if [ $(az group exists --name $RESOURCE_GROUP_NAME) = false ]; then
+        echo "Creating '${RESOURCE_GROUP_NAME}' resource group."
+        az group create -n $RESOURCE_GROUP_NAME -l $LOCATION --tags iac=setup --output none
+    fi
 
-    $0 resourcegroup storageaccount keyvault region
+    # Create the storage account (hot storage)
+    echo "Creating '${TF_STATE_STORAGE_ACCOUNT_NAME}' storage account."
+    az storage account create -g $RESOURCE_GROUP_NAME -l $LOCATION \
+    --name "${TF_STATE_STORAGE_ACCOUNT_NAME}" \
+    --sku Standard_LRS \
+    --encryption-services blob \
+    --kind StorageV2 \
+    --tags iac=setup \
+    --output none
 
-Example:
+    # Retrieve the storage account key
+    ACCOUNT_KEY=$(az storage account keys list --resource-group $RESOURCE_GROUP_NAME --account-name "${TF_STATE_STORAGE_ACCOUNT_NAME}" --query [0].value -o tsv)
 
-    $0 resourcegroup storageaccount keyvault eastus2
+    # Create a storage container (for the Terraform State)
+    az storage container create --name $TF_STATE_CONTAINER_NAME \
+    --account-name "${TF_STATE_STORAGE_ACCOUNT_NAME}" \
+    --account-key $ACCOUNT_KEY \
+    --output none
 
-EOF
-    exit 1;
-fi
+    # Create an Azure KeyVault
+    echo "Creating '${KEYVAULT_NAME}' keyvault."
+    az keyvault create -g $RESOURCE_GROUP_NAME \
+    -l $LOCATION --name $KEYVAULT_NAME \
+    --tags iac=setup --output none
 
-RESOURCE_GROUP_NAME=$1
-TF_STATE_STORAGE_ACCOUNT_NAME=$2
-KEYVAULT_NAME=$3
-LOCATION=$4
+    # Store the Terraform State Storage Key into KeyVault
+    echo "Storing tf state storage key to keyvault."
+    az keyvault secret set --name tfstate-storage-key --value $ACCOUNT_KEY --vault-name $KEYVAULT_NAME --output none
 
-export TF_STATE_CONTAINER_NAME=terraform-state
+    # Create Service Principal
+    echo "Creating service principal scoped to ${RESOURCE_GROUP_NAME} resource group."
+    SUBSCRIPTION_ID=$(az account show --query id --output tsv)
+    ad=$(az ad sp create-for-rbac -n $SP_NAME --role Contributor --scopes /subscriptions/$SUBSCRIPTION_ID/resourceGroups/$RESOURCE_GROUP_NAME --query '[appId, password]' --output tsv)
+    APP_ID=`echo "${ad}" | head -1`
+    SP_PASSWD=`echo "${ad}" | tail -1`
+    TENANT_ID=$(az ad sp show --id $APP_ID --query appOwnerTenantId --output tsv)
 
-# Create the resource group
-az group create -n "$RESOURCE_GROUP_NAME" -l "$LOCATION"
+    # Store credentials to be used by Terraform
+    echo "Storing Service Principal cred to keyvault."
+    az keyvault secret set --name tf-subscription-id --value $SUBSCRIPTION_ID --vault-name $KEYVAULT_NAME --output none
+    az keyvault secret set --name tf-sp-id --value $APP_ID --vault-name $KEYVAULT_NAME --output none
+    az keyvault secret set --name tf-sp-secret --value $SP_PASSWD --vault-name $KEYVAULT_NAME --output none
+    az keyvault secret set --name tf-tenant-id --value $TENANT_ID --vault-name $KEYVAULT_NAME --output none
+    az keyvault secret set --name tf-storage-name --value "${TF_STATE_STORAGE_ACCOUNT_NAME}" --vault-name $KEYVAULT_NAME --output none
 
-# Create the storage account for dev (hot storage)
-az storage account create -g "$RESOURCE_GROUP_NAME" -l "$LOCATION" \
-  --name "${TF_STATE_STORAGE_ACCOUNT_NAME}dev" \
-  --sku Standard_LRS \
-  --encryption-services blob \
-  --kind StorageV2
-
-# Retrieve the storage account key for dev
-ACCOUNT_KEY_DEV=$(az storage account keys list --resource-group "$RESOURCE_GROUP_NAME" \
-    --account-name "${TF_STATE_STORAGE_ACCOUNT_NAME}dev" --query [0].value -o tsv)
-
-# Create a storage container (for the Terraform State) for dev
-az storage container create --name "$TF_STATE_CONTAINER_NAME" \
-    --account-name "${TF_STATE_STORAGE_ACCOUNT_NAME}dev" \
-    --account-key "$ACCOUNT_KEY_DEV"
-
-# Create an Azure KeyVault
-az keyvault create -g "$RESOURCE_GROUP_NAME" -l "$LOCATION" --name "$KEYVAULT_NAME"
-
-# Store the Terraform State Storage Key into KeyVault
-az keyvault secret set --name tfstate-storage-key-dev --value "$ACCOUNT_KEY_DEV" --vault-name "$KEYVAULT_NAME"
+    # Display information
+    echo -e "\n"
+    echo "Run the following command to initialize Terraform to store its state into Azure Storage:"
+    echo "terraform init -backend-config=\"storage_account_name=${TF_STATE_STORAGE_ACCOUNT_NAME}\" -backend-config=\"container_name=$TF_STATE_CONTAINER_NAME\" -backend-config=\"access_key=\$(az keyvault secret show --name tfstate-storage-key --vault-name $KEYVAULT_NAME --query value -o tsv)\" -backend-config=\"key=terraform.tfstate\""
 
 
-# Create Service Principal
-echo "Creating Service Principal"
-SUBSCRIPTION_ID=$(az account show --query id --output tsv)
-ad=$(az ad sp create-for-rbac --role Contributor --scopes /subscriptions/"$SUBSCRIPTION_ID" --query '[appId, password]' --output tsv)
-APP_ID=$(echo "${ad}" | head -1)
-SP_PASSWD=$(echo "${ad}" | tail -1)
-TENANT_ID=$(az ad sp show --id "$APP_ID" --query appOwnerTenantId --output tsv)
-
-# Store credentials to be used by Terraform
-echo "Storing Service Principal"
-az keyvault secret set --name tf-subscription-id --value "$SUBSCRIPTION_ID" --vault-name "$KEYVAULT_NAME"
-az keyvault secret set --name tf-sp-id --value "$APP_ID" --vault-name "$KEYVAULT_NAME"
-az keyvault secret set --name tf-sp-secret --value "$SP_PASSWD" --vault-name "$KEYVAULT_NAME"
-az keyvault secret set --name tf-tenant-id --value "$TENANT_ID" --vault-name "$KEYVAULT_NAME"
-az keyvault secret set --name tf-storage-name --value "${TF_STATE_STORAGE_ACCOUNT_NAME}dev" --vault-name "$KEYVAULT_NAME"
-
-# Display information
-cat << EOF
-
-# This is required for the next step in the ReadMe (Step #3)
-# Initialize the Terraform state and store it in the Azure resources
-az login
-cd terraform/live/dev
-terraform init -backend-config="storage_account_name=${TF_STATE_STORAGE_ACCOUNT_NAME}dev" -backend-config="container_name=$TF_STATE_CONTAINER_NAME" -backend-config="access_key=\$(az keyvault secret show --name tfstate-storage-key-dev --vault-name $KEYVAULT_NAME --query value -o tsv)" -backend-config="key=terraform.tfstate"
-
-EOF
+    # Continue to create next env.
+    echo -e "------------------------------------ \n"
+    read -p "Any other env to create? (y/n) " answer
+    if ! [[ $answer =~ ^[Yy]$ ]]
+    then
+        break
+    fi
+done
