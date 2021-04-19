12614fb40cee002777437ddc448ae5214d928f0a
Shervyna Ruan <shervyna715@gmail.com>
2021-04-19 10:34:12 +0900

    [bugfix]move access policy into keyvault resource (#281)
    * move access policy into keyvault resource, and kv_id implicit dependency will work
    
    * move adf kv access policy to keyvault resource block to avoid conflict
    
    * change locals name
    
    Co-authored-by: Shervyna Ruan <HsuanWei.Ruan@microsoft.com>

--- e2e_samples/dataset_versioning/infra/terraform/modules/keyvault/locals.tf
+++ e2e_samples/dataset_versioning/infra/terraform/modules/keyvault/locals.tf
@@ -1,4 +1,4 @@
 locals {
-  get_permissions                       = ["Get"]
-  get_set_delete_purge_list_permissions = ["Get", "Set", "Delete", "Purge", "List", "Recover"]
+  adf_identity_secret_permissions  = ["Get"]
+  client_config_secret_permissions = ["Get", "Set", "Delete", "Purge", "List", "Recover"]
 }
