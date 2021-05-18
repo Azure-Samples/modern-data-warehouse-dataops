efdea0d2858bdb16b597e964a3eb1c66229e4237
Namit <68831618+namitms@users.noreply.github.com>
2021-05-19 08:59:29 +1000

    Update test_sparkSQL.py

--- single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
+++ single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
@@ -2,12 +2,25 @@
 # MAGIC  %pip install nutter
 
 # COMMAND ----------
-
 from runtime.nutterfixture import NutterFixture, tag
 class MyTestFixture(NutterFixture):
-   def assertion_test_name(self):
+
+   # Arrange
+   def before_test_Records_Exist_Returns_Positive_Number(self):
+      self.code1_result = dbutils.notebook.run('./testSQL', 600)  
+
+   # Act
+   def run_test_Records_Exist_Returns_Positive_Number(self):
       some_tbl = sqlContext.sql('SELECT TOTAL FROM POPULATION_COUNT LIMIT 1')
+
+   #Assert
+   def assertion_Records_Exist_Returns_Positive_Number(self):      
       assert (some_tbl.first()[0] > 1)
+
+   #Clean
+   def after_all(self):
+       sqlContext.sql('DROP TABLE POPULATION_COUNT')
+
 print("Starting Test")
 result = MyTestFixture().execute_tests()
 print(result.to_string())
