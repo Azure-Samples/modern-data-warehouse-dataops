4614b55f3c4d6dc6ea8df9da19333d50c6a43fe4
Namit <68831618+namitms@users.noreply.github.com>
2021-05-18 12:24:54 +1000

    test notebook

--- single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
+++ single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
@@ -1,12 +1,17 @@
- %pip install nutter
+# Databricks notebook source
+# MAGIC  %pip install nutter
+
+# COMMAND ----------
 
 from runtime.nutterfixture import NutterFixture, tag
 class MyTestFixture(NutterFixture):
    def assertion_test_name(self):
       some_tbl = sqlContext.sql('SELECT COUNT(*) FROM US_POPULATION')
-      assert (some_tbl.first()[0] < 1)
+      assert (some_tbl.first()[0] > 1)
 print("Starting Test")
 result = MyTestFixture().execute_tests()
 print(result.to_string())
 # Comment out the next line (result.exit(dbutils)) to see the test result report from within the notebook
 result.exit(dbutils)
+
+# COMMAND ----------
