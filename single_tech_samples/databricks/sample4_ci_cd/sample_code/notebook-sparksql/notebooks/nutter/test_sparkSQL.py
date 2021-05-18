726067dff138119d1c3ce8ac3d2019fdef32fba6
Namit <68831618+namitms@users.noreply.github.com>
2021-05-18 12:50:23 +1000

    Better tests

--- single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
+++ single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/nutter/test_sparkSQL.py
@@ -7,7 +7,7 @@ from runtime.nutterfixture import NutterFixture, tag
 class MyTestFixture(NutterFixture):
    def assertion_test_name(self):
       some_tbl = sqlContext.sql('SELECT COUNT(*) FROM US_POPULATION')
-      assert (some_tbl.first()[0] < 1)
+      assert (some_tbl.first()[0] > 1)
 print("Starting Test")
 result = MyTestFixture().execute_tests()
 print(result.to_string())
