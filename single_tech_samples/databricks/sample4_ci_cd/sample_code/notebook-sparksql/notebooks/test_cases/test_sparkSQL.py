c4101f33b9fab9a3b0101702a1ed21548a789d27
Namit <68831618+namitms@users.noreply.github.com>
2021-05-19 09:45:11 +1000

    Update test_sparkSQL.py

--- single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/test_cases/test_sparkSQL.py
+++ single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/test_cases/test_sparkSQL.py
@@ -4,22 +4,24 @@
 # COMMAND ----------
 from runtime.nutterfixture import NutterFixture, tag
 class MyTestFixture(NutterFixture):
-
+   total = 0
+    
    # Arrange
-   def before_test_Records_Exist_Returns_Positive_Number(self):
-      self.code1_result = dbutils.notebook.run('../sparkSQL_sample', 600)  
+   def before_Records_Exist_Returns_Positive_Number(self):
+      dbutils.notebook.run('../sparkSQL_sample', 600)  
 
    # Act
-   def run_test_Records_Exist_Returns_Positive_Number(self):
+   def run_Records_Exist_Returns_Positive_Number(self):      
       some_tbl = sqlContext.sql('SELECT TOTAL FROM POPULATION_COUNT LIMIT 1')
+      MyTestFixture.total = some_tbl.first()[0]
 
    #Assert
    def assertion_Records_Exist_Returns_Positive_Number(self):      
-      assert (some_tbl.first()[0] > 1)
+      assert (MyTestFixture.total > 0)
 
    #Clean
    def after_all(self):
-       sqlContext.sql('DROP TABLE POPULATION_COUNT')
+       sqlContext.sql('DROP TABLE IF EXISTS  POPULATION_COUNT;')
 
 print("Starting Test")
 result = MyTestFixture().execute_tests()
