a32f2cf353634e9a16d454ba53183d52fd608986
Namit <68831618+namitms@users.noreply.github.com>
2021-05-19 10:03:36 +1000

    More test cases

--- single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/test_cases/test_sparkSQL.py
+++ single_tech_samples/databricks/sample4_ci_cd/sample_code/notebook-sparksql/notebooks/test_cases/test_sparkSQL.py
@@ -5,26 +5,43 @@
 from runtime.nutterfixture import NutterFixture, tag
 class MyTestFixture(NutterFixture):
    total = 0
+   first_year = 0
     
    # Arrange
-   def before_Records_Exist_Returns_Positive_Number(self):
+   def before_all(self):
       dbutils.notebook.run('../sparkSQL_sample', 600)  
 
+   # ************** Test Case 1 ********************
    # Act
    def run_Records_Exist_Returns_Positive_Number(self):      
-      some_tbl = sqlContext.sql('SELECT TOTAL FROM POPULATION_COUNT LIMIT 1')
-      MyTestFixture.total = some_tbl.first()[0]
+      temp_result = sqlContext.sql('SELECT TOTAL FROM POPULATION_COUNT LIMIT 1')
+      MyTestFixture.total = temp_result.first()[0]
 
    #Assert
    def assertion_Records_Exist_Returns_Positive_Number(self):      
       assert (MyTestFixture.total > 0)
 
    #Clean
-   def after_all(self):
-       sqlContext.sql('DROP TABLE IF EXISTS  POPULATION_COUNT;')
+   def after_Records_Exist_Returns_Positive_Number(self):
+       sqlContext.sql('DROP TABLE IF EXISTS POPULATION_COUNT;')
 
-print("Starting Test")
+   # ************** Test Case 2 ********************
+   # Act
+   def run_FIRST_YEAR_RETURNS_1960(self):      
+      temp_result = sqlContext.sql('SSELECT YEAR FROM FIRST_YEARS_PUPULATION LIMIT 1')
+      MyTestFixture.first_year = temp_result.first()[0]
+
+   #Assert
+   def assertion_FIRST_YEAR_RETURNS_1960(self):      
+      assert (MyTestFixture.first_year == 1960)
+
+   #Clean
+   def after_FIRST_YEAR_RETURNS_1960(self):
+       sqlContext.sql('DROP TABLE IF EXISTS FIRST_YEARS_PUPULATION;')
+
+print("Starting Nutter tests")
 result = MyTestFixture().execute_tests()
+print("Test execution complete")
 print(result.to_string())
 # Comment out the next line (result.exit(dbutils)) to see the test result report from within the notebook
 result.exit(dbutils)
