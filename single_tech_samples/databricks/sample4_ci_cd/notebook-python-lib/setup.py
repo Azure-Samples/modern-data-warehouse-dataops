e3e44c836e2b2bcec80f6d8b2fc09a1509b8796b
Swetha Sundar <swetha.sundr@gmail.com>
2021-05-21 01:21:26 +0800

    Set the python version

--- single_tech_samples/databricks/sample4_ci_cd/notebook-python-lib/setup.py
+++ single_tech_samples/databricks/sample4_ci_cd/notebook-python-lib/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 data = {
     'name': 'mdw-dbx-nbpylib',
     'version': '0.0.1',
-    'packages': find_packages()
+    'packages': [common]
 }
 
 if __name__ == '__main__':
