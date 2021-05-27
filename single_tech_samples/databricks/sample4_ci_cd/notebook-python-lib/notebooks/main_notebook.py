52ffcfcd51e5835f4e664583f8a81b0b1825f273
Swetha Sundar <swetha.sundr@gmail.com>
2021-05-28 03:05:27 +0800

    - Add nutter test for databricks notebook with custom python libraries - Fix names of DevOps pipeline stages

--- single_tech_samples/databricks/sample4_ci_cd/notebook-python-lib/notebooks/main_notebook.py
+++ single_tech_samples/databricks/sample4_ci_cd/notebook-python-lib/notebooks/main_notebook.py
@@ -1,7 +1,10 @@
 # Databricks notebook source
 
 # COMMAND ----------
+# MAGIC %run ../common/module_a
+# MAGIC %run ../common/module_a
 
+# COMMAND ----------
 from common.module_b import int_to_str
 print(int_to_str(1000))
 
@@ -14,12 +17,12 @@ spark = SparkSession.builder.appName("example-data-aggregator").getOrCreate()
 
 test_data = [
             # pipe_id, start_time, end_time, litres_pumped
-            (1, '2021-02-01 01:05:32', '2021-02-01 01:09:13', 24),
-            (2, '2021-02-01 01:09:14', '2021-02-01 01:14:17', 41),
-            (1, '2021-02-01 01:14:18', '2021-02-01 01:15:58', 11),
-            (2, '2021-02-01 01:15:59', '2021-02-01 01:18:26', 13),
-            (1, '2021-02-01 01:18:27', '2021-02-01 01:26:26', 45),
-            (3, '2021-02-01 01:26:27', '2021-02-01 01:38:57', 15)
+            (1, '2021-02-01 01:05:32', '2021-02-01 01:09:13', 10),
+            (2, '2021-02-01 01:09:14', '2021-02-01 01:14:17', 20),
+            (1, '2021-02-01 01:14:18', '2021-02-01 01:15:58', 30),
+            (2, '2021-02-01 01:15:59', '2021-02-01 01:18:26', 40),
+            (1, '2021-02-01 01:18:27', '2021-02-01 01:26:26', 60),
+            (3, '2021-02-01 01:26:27', '2021-02-01 01:38:57', 60)
         ]
 test_data = [
 	{
@@ -30,4 +33,37 @@ test_data = [
 	} for row in test_data
 ]
 test_df = spark.createDataFrame(map(lambda x: Row(**x), test_data))
+test_df.createOrReplaceTempView("origin")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS original;
+# MAGIC CREATE TABLE IF NOT EXISTS original AS
+# MAGIC Select
+# MAGIC   pipe_id,
+# MAGIC   start_time,
+# MAGIC   end_time,
+# MAGIC   litres_pumped
+# MAGIC from
+# MAGIC   origin
+
+# COMMAND ----------
+
 output_df = get_litres_per_second(test_df)
+output_df.createOrReplaceTempView("output")
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC DROP TABLE IF EXISTS expected;
+# MAGIC CREATE TABLE IF NOT EXISTS expected AS
+# MAGIC Select
+# MAGIC   pipe_id,
+# MAGIC   total_duration_seconds,
+# MAGIC   total_litres_pumped,
+# MAGIC   avg_litres_per_second
+# MAGIC from
+# MAGIC   output
+
+# COMMAND ----------
