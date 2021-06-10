35a6efe4110d559b08030a6bbc0db5ca7a364632
Sean Ma <maye-msft@outlook.com>
2021-06-10 22:44:08 +0800

    merge hybrid to spark python and doc changes

--- single_tech_samples/databricks/sample4_ci_cd/multi-notebooks/notebooks/module_a_notebook.py
+++
@@ -1,7 +0,0 @@
-# Databricks notebook source
-from pyspark.sql import DataFrame
-from pyspark.sql.functions import col
-
-def double_price(df: DataFrame):
-  return df.select('Fruit', 'Color', (col('Price') * 2).alias('Price'))
-  
