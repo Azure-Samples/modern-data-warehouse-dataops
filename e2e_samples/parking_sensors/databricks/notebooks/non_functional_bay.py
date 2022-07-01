2ecc23a1e58c9c1bd9d78b8da935ef95c9c0cdae
subhamjalan <42665361+subhamjalan@users.noreply.github.com>
2022-07-01 14:26:22 +0530

    Update non_functional_bay.py

--- e2e_samples/parking_sensors/databricks/notebooks/non_functional_bay.py
+++ e2e_samples/parking_sensors/databricks/notebooks/non_functional_bay.py
@@ -2,12 +2,6 @@
 # MAGIC %pip install great-expectations==0.14.12
 # MAGIC %pip install opencensus-ext-azure==1.1.3
 
-# COMMAND ----------
-
-dbutils.widgets.text("loadid", "", "Load Id")
-loadid = dbutils.widgets.get("loadid")
-
-# COMMAND ----------
 
 import datetime
 import os
@@ -16,11 +10,8 @@ import ddo_transform.transform as t
 import ddo_transform.util as util
 
 load_id = loadid
-loaded_on = datetime.datetime.now()
-base_path = 'dbfs:/mnt/datalake/data/dw/'
 
-
-sensordata_non_functional_bay = spark.read.table("interim.sensor").filter(col('load_id') == lit(load_id))
+sensordata_non_functional_bay = spark.read.table("interim.sensor")
 sensordata_non_functional_bay.createOrReplaceTempView("sensordata_non_functional_bay")
 non_functional_bay=sql("""select distinct bay_id, 'non-functional' as status from sensordata_non_functional_bay order by bay_id desc limit 100""")
 util.save_overwrite_unmanaged_table(spark, non_functional_bay, table_name="dw.non_functional_bay", path=os.path.join(base_path, "non_functional_bay"))
