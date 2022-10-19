a6fa09018365216eb854ba55c3f6b1bc959a4fba
Yennifer Santos <ysantos@microsoft.com>
2022-10-19 14:17:43 +0200

    Step 3 was missing.

--- single_tech_samples/synapseserverless/synapseartifacts/workspace/scripts/Sc_Column_Level_Security.sql
+++ single_tech_samples/synapseserverless/synapseartifacts/workspace/scripts/Sc_Column_Level_Security.sql
@@ -7,17 +7,18 @@ CREATE LOGIN [AADGR<PROJECT_NAME><DEPLOYMENT_ID>] FROM EXTERNAL PROVIDER;
 CREATE USER [SynapseDbUser] FROM LOGIN [AADGR<PROJECT_NAME><DEPLOYMENT_ID>] ;
 
 /* RUN THIS STEPS with the manually created AD user , let's call it USER_2*/
-/* Step 3: Log in into Azure with  USER_2 */
-/* Step 4: Navigate to Synapse Studio: https://web.azuresynapse.net/ and make sure you are logged with USER_2)*/
-/* Step 5: Run the following query*/
+/* Step 3: Add USER_2 to AD Group AADGR<PROJECT_NAME><DEPLOYMENT_ID>*/
+/* Step 4: Log in into Azure with  USER_2 */
+/* Step 5: Navigate to Synapse Studio: https://web.azuresynapse.net/ and make sure you are logged with USER_2)*/
+/* Step 6: Run the following query*/
 select airport_fee from VW_202010
-/* Step 6: You should receive the following result: */
+/* Step 7: You should receive the following result: */
 /* The SELECT permission was denied on the column 'airport_fee' of the object 'VW_202010', database 'db_serverless', schema 'dbo'.*/
 
-/* Step 7: Run the following query on the Synapse Studio logged with USER_1  */
+/* Step 8: Run the following query on the Synapse Studio logged with USER_1  */
 GRANT SELECT ON VW_202010(VendorID,total_amount) TO SynapseDbUser;
 
-/* Step 8: Run the following query on the Synapse Studio logged with USER_2  */
+/* Step 9: Run the following query on the Synapse Studio logged with USER_2  */
 select VendorID,total_amount from VW_202010
 
-/* Step 9: You should be able to see the results as the AAD group is authorized on those 2 columns  */
+/* Step 10: You should be able to see the results as the AAD group is authorized on those 2 columns  */
