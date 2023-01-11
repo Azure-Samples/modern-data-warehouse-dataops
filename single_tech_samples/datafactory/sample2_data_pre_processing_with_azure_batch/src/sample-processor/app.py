a325991f8d93dc9092a9593601ddacb0fa44941a
priya-27 <priya.27bhimjyani@gmail.com>
2023-01-11 15:32:06 +0530

    changes for app file

--- single_tech_samples/datafactory/sample2_data_pre_processing_with_azure_batch/src/sample-processor/app.py
+++ single_tech_samples/datafactory/sample2_data_pre_processing_with_azure_batch/src/sample-processor/app.py
@@ -14,74 +14,70 @@ import subprocess
 
 
 def extractSampleBagFile(cargs):
-    listOfBagFiles = [args.rawPath]
+    
+    bagFile = args.rawPath
     bagNamePath = '/' + \
         args.rawPath.split(
             '/')[len(args.rawPath.split('/'))-1].split('.bag')[0]
-    numberOfFiles = "1"
-    count = 0
-    for bagFile in listOfBagFiles:
-        count += 1
-        print("Reading file " + str(count) +
-              " of  " + numberOfFiles + ": " + bagFile)
+ 
 
-        # access bag
-        bag = rosbag.Bag(bagFile)
-        bagContents = bag.read_messages()
-        bagName = bag.filename
+    #Access the bag file
+    bag = rosbag.Bag(bagFile)
+    bagContents = bag.read_messages()
+    print("Started reading The Bag file")
 
-        try:
-            if os.path.exists(args.extractedPath + bagNamePath):
-                continue
-            else:
-                os.mkdir(args.extractedPath + bagNamePath)
-        except Exception as e:
-            raise RuntimeError(f"Error: {e.__class__()}")
+    try:
+        if os.path.exists(args.extractedPath + bagNamePath):
+            pass
+        else:
+            os.mkdir(args.extractedPath + bagNamePath)
+    except Exception as e:
+        raise RuntimeError(f"Error: {e.__class__()}")
             
 
-        folder = args.extractedPath + bagNamePath
+    folder = args.extractedPath + bagNamePath
 
-        # get list of topics from the bag
-        listOfTopics = []
-        for topic, msg, t in bagContents:
-            if topic not in listOfTopics:
-                listOfTopics.append(topic)
+    # get list of topics from the bag
+    listOfTopics = []
+    for topic, msg, t in bagContents:
+        if topic not in listOfTopics:
+            listOfTopics.append(topic)
 
-        for topicName in listOfTopics:
-            # Create a new CSV file for each topic
-            filename = folder + '/' + \
-                topicName.replace('/', '_slash_') + '.csv'
-            with open(filename, 'w+') as csvfile:
-                filewriter = csv.writer(csvfile, delimiter=',')
-                firstIteration = True  # allows header row
-                # for each instant in time that has data for topicName
-                for subtopic, msg, t in bag.read_messages(topicName):
-                    # parse data from this instant, which is of the form of multiple lines of "Name: value\n"
-                    # - put it in the form of a list of 2-element lists
-                    msgString = str(msg)
-                    msgList = msgString.split('\n')
-                    instantaneousListOfData = []
-                    for nameValuePair in msgList:
-                        splitPair = nameValuePair.split(':')
-                        for i in range(len(splitPair)):  # should be 0 to 1
-                            splitPair[i] = splitPair[i].strip()
-                        instantaneousListOfData.append(splitPair)
-                    # write the first row from the first element of each pair
-                    if firstIteration:  # header
-                        headers = ["rosbagTimestamp"]  # first column header
-                        for pair in instantaneousListOfData:
-                            headers.append(pair[0])
-                        filewriter.writerow(headers)
-                        firstIteration = False
-                    # write the value from each pair to the file
-                    # first column will have rosbag timestamp
-                    values = [str(t)]
+    for topicName in listOfTopics:
+        # Create a new CSV file for each topic
+        filename = folder + '/' + \
+            topicName.replace('/', '_slash_') + '.csv'
+        with open(filename, 'w+') as csvfile:
+            filewriter = csv.writer(csvfile, delimiter=',')
+            firstIteration = True  # allows header row
+            # for each instant in time that has data for topicName
+            for subtopic, msg, t in bag.read_messages(topicName):
+                # parse data from this instant, which is of the form of multiple lines of "Name: value\n"
+                # - put it in the form of a list of 2-element lists
+                msgString = str(msg)
+                msgList = msgString.split('\n')
+                instantaneousListOfData = []
+                for nameValuePair in msgList:
+                    splitPair = nameValuePair.split(':')
+                    for i in range(len(splitPair)):  # should be 0 to 1
+                        splitPair[i] = splitPair[i].strip()
+                    instantaneousListOfData.append(splitPair)
+                # write the first row from the first element of each pair
+                if firstIteration:  # header
+                    headers = ["rosbagTimestamp"]  # first column header
                     for pair in instantaneousListOfData:
-                        if len(pair) > 1:
-                            values.append(pair[1])
-                    filewriter.writerow(values)
-        bag.close()
-    print("Extraction Successful! \nDone extracting all topics into respectice csv files")
+                        headers.append(pair[0])
+                    filewriter.writerow(headers)
+                    firstIteration = False
+                # write the value from each pair to the file
+                # first column will have rosbag timestamp
+                values = [str(t)]
+                for pair in instantaneousListOfData:
+                    if len(pair) > 1:
+                        values.append(pair[1])
+                filewriter.writerow(values)
+    bag.close()
+    print("Extraction Successful! \nDone extracting all topics into respective csv files")
 
 
 if __name__ == '__main__':
