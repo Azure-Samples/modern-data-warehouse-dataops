92f6927bbd441298e82d418eb0bd778b444b0dd0
Anuj Parashar <promisinganuj@gmail.com>
2024-10-23 17:34:17 +1100

    Minor changes

--- e2e_samples/fabric_dataops_sample/infra/terraform/variables.tf
+++ e2e_samples/fabric_dataops_sample/infra/terraform/variables.tf
@@ -1,30 +1,30 @@
 variable "use_cli" {
   description = "Select whether for authentication you are using a user context or other kind of identity (true/false)"
-  type = bool
+  type        = bool
 }
 
 variable "use_msi" {
   description = "Indicate whether for authentication you are using a managed credential or service principal (true/false)"
-  type = bool
+  type        = bool
 }
 
 variable "client_id" {
-  type = string
+  type        = string
   description = "The Application ID of the SPN/MI used to run this deployment and that will be granted Capacity Admin access"
 }
 
 variable "client_secret" {
-  type = string
+  type        = string
   description = "The secret of the SPN used to run this deployment."
 }
 
 variable "tenant_id" {
-  type = string
+  type        = string
   description = "Tenant ID"
 }
 
 variable "subscription_id" {
-  type = string
+  type        = string
   description = "Azure subscription ID"
 }
 
@@ -33,36 +33,30 @@ variable "base_name" {
   type        = string
 }
 
-variable "location" {
-  description = "The Azure region where the resources will be created"
-  type        = string
-  default     = "westus3"
-}
-
-variable "fabric_capacity_admin" {
-  description = "The Object ID of the group of Fabric capacity admins"
+variable "fabric_capacity_admins" {
+  description = "The list of the users (principal name) and/or service pricipals (object ID) to be added as Fabric capacity admins"
   type        = string
 }
 
-variable "fabric_workspace_admins" {
-  description = "The Object ID of the group of Fabric Workspace admins"
+variable "fabric_workspace_admin_sg_name" {
+  description = "The name of the security group of Fabric Workspace admins"
   type        = string
 }
 
-variable "rg_name" {
+variable "resource_group_name" {
   description = "The resource group name where the Azure resources will be created"
-  type = string  
+  type        = string
 }
 
 variable "create_fabric_capacity" {
   description = "A flag to indicate whether a new Fabric capacity should/should not be created"
-  type = bool
+  type        = bool
 }
 
-variable "fabric_capacity_id" {
-  description = "The ID of an existing Fabric capacity"
-  type = string
-  default = ""
+variable "fabric_capacity_name" {
+  description = "The name of the Fabric capacity"
+  type        = string
+  default     = ""
 }
 
 variable "git_organization_name" {
