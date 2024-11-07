8e1dc5e32c0dfc5ed181213cedc5c3ff7d9c3f54
Yennifer Santos <46519584+ydaponte@users.noreply.github.com>
2024-11-07 12:48:37 +0100

    Update post_create.sh

--- e2e_samples/parking_sensors/.devcontainer/post_create.sh
+++ e2e_samples/parking_sensors/.devcontainer/post_create.sh
@@ -4,7 +4,7 @@
 az devops configure --defaults organization="$AZDO_ORGANIZATION_URL" project="$AZDO_PROJECT"
 
 # Install requirements depending if devcontainer was openned at root or in parking_sensor folder.
-pip install -r /workspace/e2e_samples/parking_sensors/src/ddo_transform/requirements_dev.tx
+pip install -r /workspace/e2e_samples/parking_sensors/src/ddo_transform/requirements_dev.txt
 
 #Install New Databricks CLI
 curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh
