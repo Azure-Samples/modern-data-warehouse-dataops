4f8b883b47ee7c9fcd34b0e2f537e30f2175d81c
Yennifer Santos <ysantos@microsoft.com>
2024-11-07 12:54:29 +0000

    Installing DB CLI  Dockerfile. Rm previous install if exists

--- e2e_samples/parking_sensors/.devcontainer/post_create.sh
+++ e2e_samples/parking_sensors/.devcontainer/post_create.sh
@@ -6,6 +6,4 @@ az devops configure --defaults organization="$AZDO_ORGANIZATION_URL" project="$A
 # Install requirements depending if devcontainer was openned at root or in parking_sensor folder.
 pip install -r /workspace/e2e_samples/parking_sensors/src/ddo_transform/requirements_dev.txt
 
-#Install New Databricks CLI
-curl -fsSL https://raw.githubusercontent.com/databricks/setup-cli/main/install.sh | sh
 
