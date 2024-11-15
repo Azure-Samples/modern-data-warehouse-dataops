941a7510b25710224253a5940cdda8240f2c476c
Anuj Parashar <promisinganuj@gmail.com>
2024-11-15 14:51:50 +1100

    Adding file

--- e2e_samples/fabric_dataops_sample/infra/terraform/modules/keyvault_secret/main.tf
+++ e2e_samples/fabric_dataops_sample/infra/terraform/modules/keyvault_secret/main.tf
@@ -1,14 +1,8 @@
 resource "azurerm_key_vault_secret" "secret" {
-<<<<<<< HEAD
-  name         = var.name
-  value        = var.value
-  key_vault_id = var.key_vault_id
-=======
   name            = var.name
   value           = var.value
   key_vault_id    = var.key_vault_id
   expiration_date = var.expiration_date
   content_type    = var.content_type
   tags            = var.tags
->>>>>>> 2afc4f7bcf103231689f5fe5b302f5c46b87d507
 }
