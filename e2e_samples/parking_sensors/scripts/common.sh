962fcaea73e200b051d09210d15d32e9b1ece6ba
LiliamLeme <62876278+LiliamLeme@users.noreply.github.com>
2024-12-04 14:03:41 +0000

    Update e2e_samples/parking_sensors/scripts/common.sh
    Co-authored-by: Elena Terenzi <elenaterenzi@hotmail.com>

--- e2e_samples/parking_sensors/scripts/common.sh
+++ e2e_samples/parking_sensors/scripts/common.sh
@@ -37,6 +37,16 @@ createPipeline () {
     declare pipeline_name=$1
     declare pipeline_description=$2
     full_pipeline_name=$PROJECT-$pipeline_name
+    
+    # check if pipeline with same name exists
+    pipeline_id=$(az pipelines show --name "$full_pipeline_name" --output json 2>/dev/null | jq -r .id)
+
+    if [ -n "$pipeline_id" ]; then
+        az pipelines delete --id "$pipeline_id" --yes
+        echo "Deleted existing pipeline: $full_pipeline_name (Pipeline ID: $pipeline_id)"
+    else
+        echo "Pipeline $full_pipeline_name does not exist."
+    fi
     pipeline_id=$(az pipelines create \
         --name "$full_pipeline_name" \
         --description "$pipeline_description" \
@@ -47,18 +57,4 @@ createPipeline () {
         --skip-first-run true \
         --output json | jq -r '.id')
     echo "$pipeline_id"
-}
-
-ifexistsoverwrite() {
-    declare pipeline_name=$1
-    full_pipeline_name=$PROJECT-$pipeline_name
-    
-    pipeline_id=$(az pipelines show --name "$full_pipeline_name" --output json 2>/dev/null | jq -r .id)
-
-    if [ -n "$pipeline_id" ]; then
-        az pipelines delete --id "$pipeline_id" --yes
-        echo "Deleted existing pipeline: $full_pipeline_name (Pipeline ID: $pipeline_id)"
-    else
-        echo "Pipeline $full_pipeline_name does not exist."
-    fi
 }
