7d72af516789f7fea99810857667ceb2bedc4cf3
Lace Lofranco <lace.lofranco@gmail.com>
2024-12-24 14:59:49 +1000

    fix: tflint

--- e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/azure_devops/variable_group/main.tf
+++ e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/azure_devops/variable_group/main.tf
@@ -2,8 +2,8 @@ resource "azuredevops_variable_group" "vargroup" {
   project_id   = var.azure_devops_project_id
   name         = var.azure_devops_variable_group_name
   allow_access = true
-  
-  dynamic variable {
+
+  dynamic "variable" {
     for_each = var.azure_devops_variable_group_variables
     content {
       name  = variable.key
