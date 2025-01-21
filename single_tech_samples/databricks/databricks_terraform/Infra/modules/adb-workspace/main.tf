14d30c219e4493df0926f5b67efed0b56abfaf89
Dilmurod Makhamadaliev <dilmurodm@microsoft.com>
2025-01-21 17:27:31 -0500

    dependecy

--- single_tech_samples/databricks/databricks_terraform/Infra/modules/adb-workspace/main.tf
+++ single_tech_samples/databricks/databricks_terraform/Infra/modules/adb-workspace/main.tf
@@ -8,6 +8,6 @@ resource "azurerm_databricks_workspace" "this" {
   resource_group_name         = azurerm_resource_group.this.name
   location                    = azurerm_resource_group.this.location
   sku                         = "premium"
-  managed_resource_group_name = "${local.prefix}-ws-rg"
+  depends_on                  = [ azurerm_resource_group.this ]
 }
 
