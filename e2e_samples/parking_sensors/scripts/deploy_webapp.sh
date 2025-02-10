473b6766d8f4cd0ecf888c533479775f6093ce05
Brian Sherwin <brsherwi@microsoft.com>
2025-02-10 15:19:53 +0000

    changes per PR comments

--- e2e_samples/parking_sensors/scripts/deploy_webapp.sh
+++ e2e_samples/parking_sensors/scripts/deploy_webapp.sh
@@ -15,19 +15,17 @@
 # THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF 
 # CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER 
 # DEALINGS IN THE SOFTWARE.
+set -o errexit
+set -o pipefail
+set -o nounset
 
 pushd ./data/data-simulator
 
-zip -q data-simulator.zip app.js 
-zip -q data-simulator.zip .env
-zip -q data-simulator.zip package.json
-zip -q data-simulator.zip web.config
-zip -q -r data-simulator.zip sensors/
-zip -q -r data-simulator.zip helpers/
-zip -q -r data-simulator.zip collections/
+zip -q data-simulator.zip .env app.js package.json web.config
+zip -q -r data-simulator.zip sensors/ helpers/ collections/
 
 az webapp config appsettings set --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME" --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true
-az webapp deploy --clean --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME" --src-path ./data-simulator.zip --type zip --async true
+az webapp deploy --clean true --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME" --src-path ./data-simulator.zip --type zip --async true
 
 rm data-simulator.zip
 
