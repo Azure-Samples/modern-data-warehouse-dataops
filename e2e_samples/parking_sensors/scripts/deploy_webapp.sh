c1c9949e708d5d74717a0467fc83582ac6ebd957
Brian Sherwin <brsherwi@microsoft.com>
2025-02-12 17:46:11 +0000

    add a step to restart the webapp needed during reploy

--- e2e_samples/parking_sensors/scripts/deploy_webapp.sh
+++ e2e_samples/parking_sensors/scripts/deploy_webapp.sh
@@ -27,6 +27,10 @@ zip -q -r data-simulator.zip sensors/ helpers/ collections/
 az webapp config appsettings set --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME" --settings SCM_DO_BUILD_DURING_DEPLOYMENT=true
 az webapp deploy --clean true --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME" --src-path ./data-simulator.zip --type zip --async true
 
+# Restart the webapp to ensure the latest changes are applied
+az webapp stop --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME"
+az webapp start --resource-group "$RESOURCE_GROUP_NAME" --name "$APP_NAME"
+
 rm data-simulator.zip
 
 popd
