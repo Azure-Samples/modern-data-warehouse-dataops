bfa6feb33ce69cd24bc0e46956cd17fa30868b0f
joshuaphelps <joshuaphelps@microsoft.com>
2025-02-28 23:58:08 +0000

    revert

--- e2e_samples/unstructured_data/src/evaluators/exact_numeric_match.py
+++ e2e_samples/unstructured_data/src/evaluators/exact_numeric_match.py
@@ -5,40 +5,46 @@ from evaluators.citation_evaluator import CitationEvaluator
 
 
 class ExactNumericMatchEvaluator:
-    def __call__(self, ground_truth: str, response: str) -> float:
-        """
-        # noqa: W605
-        Exact matches numeric values in a given response against the ground truth value.
+    """
+    # noqa: W605
+    Exact matches numeric values in a given response against the ground truth value.
 
-        Evaluates the response against the ground truth numeric value.
-        Returns: A dictionary containing a "ratio" key.
-            - "ratio":
-                - 1.0 if any of the "excerpt" values in the response contain
-                the ground truth numeric value.
-                - 0.0 otherwise.
-        **Regular Expression Explanation:**
-        - `[\$]?`: Matches an optional dollar sign ($) character.
-        - `\s*`: Matches zero or more whitespace characters(spaces, tabs, newlines).
-        - `\d{1,3}`: Matches one to three digits (0-9).
-        - `(?:,\d{3})*`: Matches zero or more groups of:
-            - `,`: A comma.
-            - `\d{3}`: Three digits.
-        - `(?:\.\d+)?`: Matches an optional group of:
-            - `\.`: A decimal point.
-            - `\d+`: One or more digits.
-        This regular expression extracts potential numeric values from the text,
-        allowing for various formats:
-        - `$123`
-        - `$1,234`
-        - `$1,234,567`
-        - `$123.45`
-        - `$1,234.56`
-        - `123` (without dollar sign)
-        - `1,234` (without dollar sign)
-        - `1,234,567` (without dollar sign)
-        - `123.45` (without dollar sign)
-        - `1,234.56` (without dollar sign)
-        """
+    Args:
+        truth_key: The key within the ground truth dictionary that is being evaluated.
+
+    Methods:
+        __call__(ground_truth, repsonse):
+            Evaluates the response against the ground truth numeric value.
+            Returns: A dictionary containing a "ratio" key.
+                - "ratio":
+                    - 1.0 if any of the "excerpt" values in the response contain
+                    the ground truth numeric value.
+                    - 0.0 otherwise.
+            **Regular Expression Explanation:**
+            - `[\$]?`: Matches an optional dollar sign ($) character.
+            - `\s*`: Matches zero or more whitespace characters(spaces, tabs, newlines).
+            - `\d{1,3}`: Matches one to three digits (0-9).
+            - `(?:,\d{3})*`: Matches zero or more groups of:
+                - `,`: A comma.
+                - `\d{3}`: Three digits.
+            - `(?:\.\d+)?`: Matches an optional group of:
+                - `\.`: A decimal point.
+                - `\d+`: One or more digits.
+            This regular expression extracts potential numeric values from the text,
+            allowing for various formats:
+            - `$123`
+            - `$1,234`
+            - `$1,234,567`
+            - `$123.45`
+            - `$1,234.56`
+            - `123` (without dollar sign)
+            - `1,234` (without dollar sign)
+            - `1,234,567` (without dollar sign)
+            - `123.45` (without dollar sign)
+            - `1,234.56` (without dollar sign)
+    """
+
+    def __call__(self, ground_truth: str, response: str) -> float:
         numbers_regex = r"[\$]?\s*\d{1,3}(?:,\d{3})*(?:\.\d+)?"
         numbers = re.findall(numbers_regex, response)
         truth_numbers = re.findall(numbers_regex, ground_truth)
