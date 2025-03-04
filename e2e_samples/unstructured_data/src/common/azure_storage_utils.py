23bde0f882d70e093239d77ce6779f07b7185882
Jeffrey Feng <jeffeng@microsoft.com>
2025-03-04 21:50:25 +0000

    more updates

--- e2e_samples/unstructured_data/src/common/azure_storage_utils.py
+++ e2e_samples/unstructured_data/src/common/azure_storage_utils.py
@@ -1,7 +1,5 @@
-from azure.identity import DefaultAzureCredential
 from azure.storage.blob import BlobServiceClient
 
 
-def get_blob_service_client(account_url: str) -> BlobServiceClient:
-    credential = DefaultAzureCredential()
-    return BlobServiceClient(account_url=account_url, credential=credential)
+def get_blob_service_client(conn_string: str) -> BlobServiceClient:
+    return BlobServiceClient.from_connection_string(conn_string)
