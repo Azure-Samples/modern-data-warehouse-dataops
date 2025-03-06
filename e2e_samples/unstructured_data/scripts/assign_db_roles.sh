8d368174b3f8084a3750176dd8997af27f4888b9
Yomi Lajide <yolajide@microsoft.com>
2025-03-06 17:00:40 -0500

    address role creattion error

--- e2e_samples/unstructured_data/scripts/assign_db_roles.sh
+++ e2e_samples/unstructured_data/scripts/assign_db_roles.sh
@@ -6,16 +6,13 @@ SQL_SERVER=$2
 DATABASE=$3
 FUNCTION_APP=$4
 
-# Get Principal ID of the Managed Identity
-PRINCIPAL_ID=$(az webapp show --name $FUNCTION_APP --resource-group $RESOURCE_GROUP --query identity.principalId -o tsv)
-
-echo "Principal ID: $PRINCIPAL_ID"
+echo "Principal: $FUNCTION_APP"
 echo "sql server: $SQL_SERVER"
 echo "database: $DATABASE"
 
 # Run SQL commands to assign roles
 sqlcmd -S "$SQL_SERVER.database.windows.net" -d $DATABASE -G -l 30 -Q "
-    CREATE USER [$PRINCIPAL_ID] FROM EXTERNAL PROVIDER;
-    ALTER ROLE db_datareader ADD MEMBER [$PRINCIPAL_ID];
-    ALTER ROLE db_datawriter ADD MEMBER [$PRINCIPAL_ID];
+    CREATE USER [$FUNCTION_APP] FROM EXTERNAL PROVIDER;
+    ALTER ROLE db_datareader ADD MEMBER [$FUNCTION_APP];
+    ALTER ROLE db_datawriter ADD MEMBER [$FUNCTION_APP];
 "
