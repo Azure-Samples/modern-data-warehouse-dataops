5e4ca3721108bace37260e48aec4a6354385d756
Yennifer Santos <ysantos@microsoft.com>
2025-03-07 10:40:25 +0100

    fix: explore notebook files update

--- e2e_samples/parking_sensors/databricks/notebooks/01_explore.py
+++ e2e_samples/parking_sensors/databricks/notebooks/01_explore.py
@@ -4,11 +4,14 @@ import datetime
 
 # For testing
 # Replace values before testing
-stgaccountname=""
-existing_data=""
-base_path = f"abfss://datalake@{stgaccountname}.dfs.core.windows.net/data/lnd/{existing_data}"
-parkingbay_filepath = os.path.join(base_path, "MelbParkingBayData.json")
-sensors_filepath = os.path.join(base_path, "MelbParkingSensorData.json")
+#infilefolder = '2022_03_23_10_28_02/'
+#stgaccountname = "stg_account_name"
+
+abfss_path= f"""abfss://datalake@{stgaccountname}.dfs.core.windows.net/data/lnd/"""
+base_path = os.path.join(abfss_path,infilefolder)
+parkingbay_filepath = os.path.join(base_path, "ParkingLocationData.json")
+sensors_filepath = os.path.join(base_path, "ParkingSensorData.json")
+
 
 # COMMAND ----------
 
