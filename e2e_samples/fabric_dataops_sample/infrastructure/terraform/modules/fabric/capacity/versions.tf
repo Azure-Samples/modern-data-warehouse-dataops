74b13f3f330b211ddf645db0f1d364c66f834b1a
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-03-19 00:11:54 -0700

    build(fabric_dataops_sample): bump versions, mark secrets in azdo pipelines (#1175)
    * build(fabric_dataops_sample): bump versions, mark secrets in azdo pipelines
    
    * chore: update checkout action to v4 in workflows
    
    * chore: update actions in code quality checks workflow

--- e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/fabric/capacity/versions.tf
+++ e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/fabric/capacity/versions.tf
@@ -4,11 +4,11 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "4.14.0"
+      version = "4.23.0"
     }
     fabric = {
       source  = "microsoft/fabric"
-      version = "0.1.0-beta.8"
+      version = "0.1.0-rc.1"
     }
   }
 }
