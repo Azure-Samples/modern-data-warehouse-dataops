0f859e07d082aa1ede623cdb37d9d59b9d6d5837
Dariusz Porowski <3431813+DariuszPorowski@users.noreply.github.com>
2025-03-31 13:00:24 -0700

    build(deps): bump fabric terraform provider to 1.0.0

--- e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/fabric/workspace_role_assignment/main.tf
+++ e2e_samples/fabric_dataops_sample/infrastructure/terraform/modules/fabric/workspace_role_assignment/main.tf
@@ -1,6 +1,8 @@
 resource "fabric_workspace_role_assignment" "role_assigment" {
-  workspace_id   = var.workspace_id
-  principal_id   = var.principal_id
-  principal_type = var.principal_type
-  role           = var.role
+  workspace_id = var.workspace_id
+  principal = {
+    id   = var.principal_id
+    type = var.principal_type
+  }
+  role = var.role
 }
